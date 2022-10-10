4996bda09b7edaa274e31b5e4f1dcfbdcf22e536
Javi H. Gil <javihgil@softspring.es>
2022-10-10 11:22:06 +0200

    Fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'softspring/components',
+        'pretty_version' => '5.0.x-dev',
+        'version' => '5.0.9999999.9999999-dev',
+        'reference' => '5d5b6418f2d7cb572777e6131870392af2d7ccdd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'softspring/components' => array(
+            'pretty_version' => '5.0.x-dev',
+            'version' => '5.0.9999999.9999999-dev',
+            'reference' => '5d5b6418f2d7cb572777e6131870392af2d7ccdd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
